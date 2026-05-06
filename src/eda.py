1f9cd92cf9495e9b9c9855f1c540179ffad2c13a
kamalesh2602 <kamaleshg2602@gmail.com>
2026-05-06 18:55:43 +0530

    comments on eda

--- src/eda.py
+++ src/eda.py
@@ -13,10 +13,20 @@ plt.title("Carat vs Price")
 plt.savefig("../outputs/carat_price.png")
 plt.clf()
 
+
+plt.scatter(df['depth'], df['price'])
+plt.xlabel("Depth")
+plt.ylabel("Price")
+plt.title("Depth vs Price")
+plt.savefig("../outputs/depth_price.png")
+plt.clf()
+#Scatter plot shows that price increases with carat and for depth only a certain range of depth price increases and then decreases.
+
+
 # Heatmap
 plt.figure(figsize=(10,8))
 sns.heatmap(df.corr(numeric_only=True), annot=True)
 plt.title("Correlation Heatmap")
 plt.savefig("../outputs/heatmap.png")
-
+#heatmap shows that carat has the highest correlation with price, followed by x, y, and z dimensions. Depth and table have low correlation with price.
 print("EDA Completed")
